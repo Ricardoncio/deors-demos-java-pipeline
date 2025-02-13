4339b9d439154acb3e1ddf447a65864aad7f3d6d
Ricardo <rsolo719@gmail.com>
2025-02-13 12:06:51 +0100

    otro cambio mas

--- src/main/java/deors/demos/java/pipeline/HelloService.java
+++ src/main/java/deors/demos/java/pipeline/HelloService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
 
-    @Value("Hello!")
+    @Value("${defaultGreeting:Hello!}")
     private String defaultGreeting;
 
     public HelloService() {
